1b1a7240daa532a16f5eabf2d6f31b50421fb73a
Cesar Ciello <cesarciello15@gmai.com>
2020-05-08 10:38:49 -0300

    Updates

---
+++ precache-manifest.4f769ccd0dcafebc3e2033840d2613c5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b1fb19147ff71e1f4cb53f4ddce566",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44827dfde4d5b2fe1c9e",
+    "url": "/static/css/main.63c82b5f.chunk.css"
+  },
+  {
+    "revision": "7d3d7728599a9b9c1207",
+    "url": "/static/js/2.91f68658.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.91f68658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44827dfde4d5b2fe1c9e",
+    "url": "/static/js/main.f18c4ca8.chunk.js"
+  },
+  {
+    "revision": "0bdaa3f5b05c53629baa",
+    "url": "/static/js/runtime-main.ecd94e98.js"
+  },
+  {
+    "revision": "cdc4a1a58ed7d36ad2cdec02087ddcd6",
+    "url": "/static/media/Expirience.cdc4a1a5.svg"
+  },
+  {
+    "revision": "023ee584e7a96954ef8feaa57da156b3",
+    "url": "/static/media/Home.023ee584.svg"
+  },
+  {
+    "revision": "296293e2be900304ac70afafc1511230",
+    "url": "/static/media/Project.296293e2.svg"
+  },
+  {
+    "revision": "2ae3a8ddaa22ec174e907a6415ad2da6",
+    "url": "/static/media/Talk.2ae3a8dd.svg"
+  },
+  {
+    "revision": "309c0b48ef6ebdecf378e097e7d6f710",
+    "url": "/static/media/Tec.309c0b48.svg"
+  },
+  {
+    "revision": "6a9ae1679a2d3e909516b958e9f01605",
+    "url": "/static/media/perfil.6a9ae167.jpg"
+  }
+]);
